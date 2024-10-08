e4c27e76fe1184b3bf351be12267c1a4f0210c8f
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-08 12:35:05 -0300

    feat: Creating posts controller and service

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsOptional, IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
   @IsString()
@@ -8,6 +8,6 @@ export class CreatePostDto {
   @IsOptional()
   content: string;
 
-  @IsEmail()
-  authorEmail: string;
+  @IsString()
+  authorId: string;
 }
