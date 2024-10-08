454671e5015003ac47bca81e225795d77f84676d
Geraldo Luiz <geraldobl58@gmail.com>
2024-10-08 11:37:37 -0300

    feat: Creating data validation when creating and editing posts

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsOptional, IsString } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString()
   title: string;
+
+  @IsString()
+  @IsOptional()
   content: string;
+
+  @IsEmail()
   authorEmail: string;
 }
